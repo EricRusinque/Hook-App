6abc4e831a00dcf0c7a6ac2290ac11794cbd5b80
unknown <emprenderusinque@hotmail.com>
2022-01-25 14:50:29 +0100

    First use useRef

--- src/index.jsx
+++ src/index.jsx
@@ -5,13 +5,15 @@ import ReactDOM from 'react-dom';
 // import { CounterWithCustomHook } from './components/01-useState/CounterWithCustomHook';
 // import { SimpleForm } from './components/02-useEffect/SimpleForm';
 // import { FormWithCustomHook } from './components/02-useEffect/FormWithCustomHook';
-import { MultipleCustomHooks } from './components/03-examples/MultipleCustomHooks';
+// import { MultipleCustomHooks } from './components/03-examples/MultipleCustomHooks';
+import { FocusScreen } from './components/04-useRef/FocusScreen';
+
 
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <MultipleCustomHooks/>
+    <FocusScreen/>
   </React.StrictMode>,
   document.getElementById('root')
 );
