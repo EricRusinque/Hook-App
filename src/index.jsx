e99e45b790a321e9c791d3b5f3d9e0a104dd8df8
unknown <emprenderusinque@hotmail.com>
2022-01-26 02:18:11 +0100

    exercise with memo and useCallback

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { CallbackHook } from './components/06-memos/CallbackHook';
-import { MemoHook } from './components/06-memos/MemoHook';
+// import { CallbackHook } from './components/06-memos/CallbackHook';
+// import { MemoHook } from './components/06-memos/MemoHook';
 // import { HookApp } from './HookApp';
 // import { CounterApp } from './components/01-useState/CounterApp';
 // import { CounterWithCustomHook } from './components/01-useState/CounterWithCustomHook';
@@ -12,13 +12,14 @@ import { MemoHook } from './components/06-memos/MemoHook';
 // import { RealExampleRef } from './components/04-useRef/RealExampleRef';
 // import { Layout } from './components/05-useLayoutEffect/Layout';
 // import { Memorize } from './components/06-memos/Memorize';
+import { Padre } from './components/07-tarea-memo/Padre';
 
 
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <CallbackHook/>
+    <Padre/>
   </React.StrictMode>,
   document.getElementById('root')
 );
